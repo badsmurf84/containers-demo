92c9ce09c189ae46bf91169862bf81e62d635573
Scott Horsfield <horsfieldsa@gmail.com>
2018-04-12 21:13:16 -0700

    Updates how data is displayed

--- application.py
+++ application.py
@@ -9,26 +9,41 @@ import json
 app = Flask(__name__)
 
 # Get Information on Environment - And Task Metadata if Environment is ECS
-def get_task_metadata(environment):
+def get_task_metadata(platform):
     try:
-      if environment == "ECS":
+      if platform == "ECS":
           response = requests.get('http://169.254.170.2/v2/metadata')
           if response.status_code == 200:
               return json.dumps(response.json(), sort_keys = True, indent = 4, separators = (',', ': '))
           else:
               return "The application is running in ECS but it could not connect to the metadata endpoint."
-      elif environment == "DOCKER":
-        return "The application is running locally in a Docker container."
-      elif environment == "LOCAL":
-        return "The application is running locally."
     except:
       return "APP_ENV is not correctly defined as an environment variable. Ensure APP_ENV is set to LOCAL, DOCKER or ECS."
+
+def get_platform_message(platform):
+    try:
+      if platform == "ECS":
+          return "The application is running on ECS."
+      elif platform == "LOCAL":
+          return "The application is running locally."
+      elif platform == "DOCKER":
+          return "The applicatipn is running locally in a container."
+    except:
+      return "APP_ENV is not correctly defined as an environment variable. Ensure APP_ENV is set to LOCAL, DOCKER or ECS."
+
+def get_service_endpoints():
+    try:
+        return ""
+    except:
+        return "Unable to find any service endpoints."
 
 @app.route('/')
 def hello():
     message = "Welcome to the Containers Immersion Day!"
+    platform = get_platform_message(os.environ['APP_ENV'])
     metadata = get_task_metadata(os.environ['APP_ENV'])
-    return render_template('application.html', message=message, metadata=metadata)
+    endpoints = get_service_endpoints()
+    return render_template('application.html', message=message, platform=platform, metadata=metadata, endpoints=endpoints)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
